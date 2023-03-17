a736a1028fc339059641dda40120fc5d92e06ea8
タイムライン <53483352+Nep-Timeline@users.noreply.github.com>
2023-03-17 22:14:22 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args)
     {
+        System.setProperty("java.net.useSystemProxies", "true");
         SwingUtilities.invokeLater(() -> new UI().initComponent());
     }
 }
